f5ebec2aba17db43049d94690f282f3d2f8a2e54
MarkMusili <musiliyrn@gmail.com>
2023-06-25 20:27:52 +0000

    Implimentation for d and i

--- specifiers.c
+++ specifiers.c
@@ -1,43 +1,32 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * print_c - prints a character
+ * print_d - Prints an integer
  * @args: arguments given
  * @len: Number of bytes printed
- * Return: Number of characters printed
+ * Return: Number of arguments printed.
  */
-int print_c(va_list args, unsigned long long *len)
+int print_d(va_list args, unsigned long long *len)
 {
-	char c;
+	int *d, count = 0;
 
-	c = (char)va_arg(args, int);
-	if (c == NULL)
-		c = '\0';
-	_putchar(c);
-	(*len)++;
-	return (1);
-}
-
-/**
- * print_s - Prints a string
- * @args: arguments given
- * @len: Number of bytes printed.
- * Return: Number of characters printed
- */
-int print_s(va_list args, unsigned long long *len)
-{
-	char *s;
-	int count = 0;
-
-	s = va_arg(args, char *);
-	if (s == NULL)
-		s = "";
-	while (*s != '\0')
+	*d = va_arg(args, int *);
+	while (d != NULL && *d != '\0')
 	{
-		_putchar(*s);
-		s++;
+		_putchar(*d);
 		count++;
 	}
-	*len += count;
+	*len = count;
 	return (count);
+
+}
+/**
+ * print_i - Prints a decimal
+ * @args: arguments given
+ * @len: Number of bytes printed
+ * Return: Number of arguments given.
+ */
+int print_i(va_list args, unsigned long long *len)
+{
+	return (print_d(args));
 }
