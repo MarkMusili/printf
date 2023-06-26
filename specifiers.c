7b6e9830046248c3b09c37573e2d53fdd7e6dff5
Mohammed Mubarak <modymu9@gmail.com>
2023-06-26 15:25:43 +0200

    fix issue

--- specifiers.c
+++ specifiers.c
@@ -1,6 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
-#include <stddef>
+#include <stddef.h>
 #include <stdarg.h>
 
 /**
@@ -13,8 +13,6 @@ int print_c(va_list args, unsigned long long *len)
 {
 	char c;
 
-	if (!args)
-		return (-1);
 	(*len)++;
 	c = va_arg(args, int);
 	return (_putchar(c));
@@ -31,8 +29,6 @@ int print_s(va_list args, unsigned long long *len)
 	char *s;
 	int i = 0;
 
-	if (!args)
-		return (-1);
 	s = va_arg(args, char *);
 	if (s == NULL)
 		return (-1);
