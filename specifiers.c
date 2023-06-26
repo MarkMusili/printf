8a4590f1d6fe5aa9e79f4d31c7d3cc688fc178bf
Mohammed Mubarak <modymu9@gmail.com>
2023-06-26 10:08:23 +0200

    added task 1 print_d and print_i functions

--- specifiers.c
+++ specifiers.c
@@ -44,3 +44,57 @@ int print_s(va_list args, unsigned long long *len)
 	*len += i;
 	return (i);
 }
+
+/**
+ * print_d - print an integer
+ * @args: number to be printed
+ * @len: length of the format
+ * Return: length of the variable or -1
+ */
+int print_d(va_list args, unsigned long long *len)
+{
+	long long d;
+	char *dtos;
+	int i = 0;
+
+	if (!args)
+		return (-1);
+	d = va_arg(args, int);
+	if (d == NULL)
+		return (-1);
+	dtos = _itos(d);
+	while (dtos[i])
+	{
+		_putchar(dtos[i]);
+		i++;
+	}
+	*len += i;
+	return (i);
+}
+
+/**
+ * print_i - print an decimal number
+ * @args: number to be printed
+ * @len: length of the format
+ * Return: length of the variable or -1
+ */
+int print_i(va_list args, unsigned long long *len)
+{
+	double i;
+	char *itos;
+	int k = 0;
+
+	if (!args)
+		return (-1);
+	i = va_arg(args, double);
+	if (i == NULL)
+		return (-1);
+	itos = _itos(d);
+	while (itos[k])
+	{
+		_putchar(itos[k]);
+		k++;
+	}
+	*len += k;
+	return (k);
+}
